ead7d5a9fd1e6c8b5b0e4e1b9ade040af509afdd
guilogar <jdkdejava@gmail.com>
2018-10-10 17:21:03 +0200

    Proyecto de la semana 2 con unity en curso.

---
+++ Semana2/Assets/Jugador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+    public Camera camara;
+    public float force = 1;
+
+    private Vector3 offset;
+    private Rigidbody rb;
+    private bool changeDir;
+    private Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Semana2/Assets/Pulsar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pulsar : MonoBehaviour {
+
+    int mostrar;
+    bool count;
+    Button elboton;
+    Image myImage;
+    public Sprite unoImage;
+    public Sprite dosImage;
+    public Sprite tresImage;
+    private float myDealy = 1;
+
+	// Use this for initialization
+	void Start () {
+        count = false;
+        elboton = GetComponent<Button>();
+        myImage = GetComponent<Image>();
+        elboton.onClick.AddListener(BotonPulsado);
+        mostrar = 3;
+	}
+	
+    void BotonPulsado()
+    {
+        count = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if(count)
+        {
+            switch(mostrar)
+            {
+                case 0: SceneManager.LoadScene(1); break;
+                case 1: myImage.sprite = unoImage; break;
+                case 2: myImage.sprite = dosImage; break;
+                case 3: myImage.sprite = tresImage; break;
+            }
+
+            StartCoroutine(Esperar());
+            mostrar--;
+            count = false;
+        }
+    }
+
+    IEnumerator Esperar()
+    {
+        yield return new WaitForSeconds(myDealy);
+        count = true;
+    }
+}
